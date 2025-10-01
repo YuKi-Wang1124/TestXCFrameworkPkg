7b89bf5f99eb10ea4f2af4297c1472dcf1882783
779-A25107-MAC <wkiki1124@gmail.com>
2025-10-01 10:13:30 +0800

    chore

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TestXCFramework", // 必須等於 .xcframework 內的 Module 名
-            url: "https://github.com/YuKi-Wang1124/TestXCFrameworkPkg/releases/download/1.0.0/TestXCFramework.xcframework.zip",
+            url: "https://github.com/YuKi-Wang1124/TestXCFrameworkPkg/releases/download/v1.0.0/TestXCFramework.xcframework.zip",
             checksum: "beea6040b02f9c22e1c6db6bc046cd99ab738cfc600ae17b6dda280b7123c571"
         ),
         .testTarget(
